d711803fb007615168cffe762633356921bdc31f
DelonC <delon.chan@go.shoreline.edu>
2025-05-21 11:14:16 -0700

    HTML manager and queue

--- HTMLManager.java
+++ HTMLManager.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
 public class HTMLManager {
-  private Queue<HTMLTag> tags;
+   private Queue<HTMLTag> tags;
   
+   public HTMLManager(Queue<HTMLTag> html) {
+      if(html == null) {
+         throw new IllegalArgumentException("HTML queue annot be empty null");
+      }
+      
+      Queue<HTMLTag> tags = new LinkedList<>();
+      while(!html.isEmpty()) {
+         tags.add(html.remove());
+      }
+   }
+   
+   public Queue<HTMLTag> getTags() {
+      return tags;
+   }
+   
 }
